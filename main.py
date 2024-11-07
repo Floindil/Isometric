6bce74e88c8b87a99bb3db06d0dd7cb33231ae06
Celin Villars <cvillars@arttence.com>
2024-11-07 13:32:38 +0100

    add box

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import pygame
 import math
 
+squash_factor = 4
+        
+def calc_isometric_rect_points(rect: pygame.Rect, y_offset = 0):
+    p1 = [rect.x+rect.width/2, rect.y - y_offset]
+    p2 = [rect.x+rect.width, rect.y+rect.height/squash_factor - y_offset]
+    p3 = [rect.x+rect.width/2, rect.y+rect.height*2/squash_factor - y_offset]
+    p4 = [rect.x, rect.y+rect.height/squash_factor - y_offset]
+    return [p1, p2, p3, p4]
+
+def draw_rect_isometric(surface: pygame.Surface, rect: pygame.Rect):
+    points = calc_isometric_rect_points(rect)
+    pygame.draw.lines(surface, "red", True, points)
+
+class Box:
+    def __init__(self, x: int, y: int, width: int, lenght: int, height: int) -> None:
+        self.rect = pygame.Rect(x, y, width, lenght)
+        self.height = height
+
+    def draw(self, surface):
+        lower_rect = calc_isometric_rect_points(self.rect)
+        upper_rect = calc_isometric_rect_points(self.rect, self.height*2/squash_factor)
+        pygame.draw.lines(surface, "red", True, lower_rect)
+        pygame.draw.lines(surface, "red", True, upper_rect)
+        pygame.draw.line(surface, "red", lower_rect[0], upper_rect[0])
+        pygame.draw.line(surface, "red", lower_rect[1], upper_rect[1])
+        pygame.draw.line(surface, "red", lower_rect[2], upper_rect[2])
+        pygame.draw.line(surface, "red", lower_rect[3], upper_rect[3])
+
 pygame.init()
 
 screen_size = (1000, 500)
@@ -9,6 +37,13 @@ pygame.display.set_caption("Isometric")
 
 surface = pygame.Surface(screen_size)
 surface.fill("light grey")
+
+rect = pygame.Rect(100,100,100,100)
+
+box1 = Box(300, 300, 100, 100, 50)
+box2 = Box(350, 325, 100, 100, 50)
+box3 = Box(350, 275, 100, 100, 50)
+box4 = Box(400, 300, 100, 100, 50)
 
 clock = pygame.time.Clock()
 
@@ -48,9 +83,13 @@ while running:
 
     screen.fill("light grey")
 
-    #pygame.draw.rect(screen, (255, 0, 0), (location[0]-25, location[1]-25, 50, 50))
     pygame.draw.circle(screen,(255, 0, 0), (location[0], location[1]), 10)
     pygame.draw.line(screen, pygame.Color("green"), location, target)
+    draw_rect_isometric(screen, rect)
+    box1.draw(screen)
+    box2.draw(screen)
+    box3.draw(screen)
+    box4.draw(screen)
 
     pygame.display.flip()
 
